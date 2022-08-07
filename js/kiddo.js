96a02c4525633e7f09df19a9a0507974fe72cf39
Clupai8o0 <clupaio4@gmail.com>
2022-08-07 12:10:19 +0300

    transitions

--- js/kiddo.js
+++ js/kiddo.js
@@ -7,7 +7,11 @@ svg.attr("width", 960).attr("height", 540);
 
 const pathsGroup = svg.append("g").attr("class", "paths");
 
-const rankScale = d3.scalePow().exponent(0.25).domain([1, 1000]).range([20, 500]);
+const rankScale = d3
+	.scalePow()
+	.exponent(0.25)
+	.domain([1, 1000])
+	.range([20, 500]);
 const dateScale = d3.scaleLinear().domain([1880, 2010]).range([80, 915]);
 
 const rankAxis = d3
@@ -26,6 +30,13 @@ const line = d3
 	.defined((d) => d !== 0)
 	.curve(d3.curveCardinal.tension(0.5));
 
+const flatLine = d3
+	.line()
+	.x((_, i) => dateScale(1880 + 10 * i))
+	.y((d) => rankScale(1000))
+	.defined((d) => d !== 0)
+	.curve(d3.curveCardinal.tension(0.5));
+
 svg.append("g").attr("transform", "translate(60, 0)").call(rankAxis);
 svg.append("g").attr("transform", "translate(0, 520)").call(dateAxis);
 
@@ -39,8 +50,20 @@ function search(name = "") {
 			.enter()
 			.append("path")
 			.attr("class", (d) => d.sex)
+			.attr("d", (d) => flatLine(d.rank))
+			.style("opacity", 0)
+			.transition()
+			.duration(1000)
+			.style("opacity", 1)
 			.attr("d", (d) => line(d.rank));
-		lines.exit().remove();
+		lines
+			.exit()
+			.style("opacity", 1)
+			.transition()
+			.duration(1000)
+			.style("opacity", 0)
+			.attr("d", (d) => flatLine(d.rank))
+			.remove();
 	} else alert(`No results for ${name}`);
 }
 
